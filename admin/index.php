6461c74f1a1f30d24998a2c358a73ffaabfd4071
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2021-03-14 16:51:54 +0100

    shtimi i faqes kryesore te adminit

---
+++ admin/index.php
@@ -0,0 +1,84 @@
+<?php include('partial/logo-part.php'); ?>
+ 
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Faqja Kryesore</h1>
+                <br><br>
+                <?php 
+                    if(isset($_SESSION['login']))
+                    {
+                        echo $_SESSION['login'];
+                        unset($_SESSION['login']);
+                    }
+                ?>
+                <br><br>
+
+                <div class="col-4 text-center">
+
+                    <?php 
+                        $sql = "SELECT * FROM kategoria";
+                        $res = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($res);
+                    ?>
+
+                    <h1><?php echo $count; ?></h1>
+                    <br />
+                    Kategorite
+                </div>
+
+                <div class="col-4 text-center">
+
+                    <?php 
+                        //Sql Query 
+                        $sql2 = "SELECT * FROM ushqimi";
+                        //Execute Query
+                        $res2 = mysqli_query($conn, $sql2);
+                        //Count Rows
+                        $count2 = mysqli_num_rows($res2);
+                    ?>
+
+                    <h1><?php echo $count2; ?></h1>
+                    <br />
+                    Ushqimet
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <?php 
+                        $sql3 = "SELECT * FROM porosia";
+                        $res3 = mysqli_query($conn, $sql3);
+                        $count3 = mysqli_num_rows($res3);
+                    ?>
+
+                    <h1><?php echo $count3; ?></h1>
+                    <br />
+                    Porosite ne Total
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <?php 
+                      
+                        $sql4 = "SELECT SUM(total) AS total FROM porosia WHERE status='E Derguar'";
+
+                        $res4 = mysqli_query($conn, $sql4);
+
+                        $row4 = mysqli_fetch_assoc($res4);
+                        
+                        $total_revenue = $row4['total'];
+
+                    ?>
+
+                    <h1>$<?php echo $total_revenue; ?></h1>
+                    <br />
+                    Revenue Generated
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </div>
+        <!-- Main Content Setion Ends -->
+<br><br>
+<?php include('partial/footer.php') ?>
