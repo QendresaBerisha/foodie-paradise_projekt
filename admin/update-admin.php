a5488e982cb92c623144d4a617adcac5a4bb529f
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2021-03-14 05:14:27 +0100

    shtimi i update admin

---
+++ admin/update-admin.php
@@ -0,0 +1,106 @@
+<?php include('partial/logo-part.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Perditesimi i Admineve</h1>
+
+        <br><br>
+
+        <?php 
+            //1. Get the ID of Selected Admin
+            $id=$_GET['id'];
+
+            //2. Create SQL Query to Get the Details
+            $sql="SELECT * FROM tbl_admin WHERE id=$id";
+
+            $res=mysqli_query($conn, $sql);
+
+            //Check whether the query is executed or not
+            if($res==true)
+            {
+                // Check whether the data is available or not
+                $count = mysqli_num_rows($res);
+                //Check whether we have admin data or not
+                if($count==1)
+                {
+                   
+                    $row=mysqli_fetch_assoc($res);
+
+                    $emri = $row['emri'];
+                    $username = $row['username'];
+                }
+                else
+                {
+                    //Redirect to Manage Admin Page
+                    header('location:'.SITEURL.'admin/manage-admin.php');
+                }
+            }
+        
+        ?>
+      <form action="" method="POST">
+
+            <table class="tbl-30">
+                <tr>
+                    <td>Emri Mbiemri: </td>
+                    <td>
+                        <input type="text" name="emri" value="<?php echo $emri; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Username: </td>
+                    <td>
+                        <input type="text" name="username" value="<?php echo $username; ?>">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                        <input type="submit" name="submit" value="Update Admin" class="btn-secondary">
+                    </td>
+                </tr>
+
+            </table>
+
+        </form>
+    </div>
+</div>
+
+<?php 
+
+    if(isset($_POST['submit']))
+    {
+        $id = $_POST['id'];
+        $emri = $_POST['emri'];
+        $username = $_POST['username'];
+
+        //Create a SQL Query to Update Admin
+        $sql = "UPDATE tbl_admin SET
+        emri = '$emri',
+        username = '$username' 
+        WHERE id='$id'
+        ";
+
+        $res = mysqli_query($conn, $sql);
+
+        //Check whether the query executed successfully or not
+        if($res==true)
+        {
+            //Query Executed and Admin Updated
+            $_SESSION['update'] = "<div class='success'>Admini eshte perditesuar me sukses!</div>";
+            //Redirect to Manage Admin Page
+            header('location:'.SITEURL.'admin/manage-admin.php');
+        }
+        else
+        {
+            //Failed to Update Admin
+            $_SESSION['update'] = "<div class='error'>Kemi deshtuar per te perditesuar Adminin.</div>";
+            //Redirect to Manage Admin Page
+            header('location:'.SITEURL.'admin/manage-admin.php');
+        }
+    }
+
+?>
+
+
+<?php include('partial/footer.php'); ?>
