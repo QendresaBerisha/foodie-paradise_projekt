c186bf53e7e5cb551181d7d9090f8a4e8dddf3e7
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2021-03-14 16:57:38 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -5,13 +5,7 @@
             <div class="wrapper">
                 <h1>Faqja Kryesore</h1>
                 <br><br>
-                <?php 
-                    if(isset($_SESSION['login']))
-                    {
-                        echo $_SESSION['login'];
-                        unset($_SESSION['login']);
-                    }
-                ?>
+               
                 <br><br>
 
                 <div class="col-4 text-center">
