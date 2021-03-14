36fac502bd6463b82e04a2ae88e2707f96516bde
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2021-03-14 03:25:48 +0100

    shtimi i manage category

---
+++ admin/manage-category.php
@@ -0,0 +1,155 @@
+<?php include('partial/logo-part.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Menaxhimi i Kategorive</h1>
+
+        <br /><br />
+        <?php 
+        
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add'];
+                unset($_SESSION['add']);
+            }
+
+            if(isset($_SESSION['remove']))
+            {
+                echo $_SESSION['remove'];
+                unset($_SESSION['remove']);
+            }
+
+            if(isset($_SESSION['delete']))
+            {
+                echo $_SESSION['delete'];
+                unset($_SESSION['delete']);
+            }
+
+            if(isset($_SESSION['no-category-found']))
+            {
+                echo $_SESSION['no-category-found'];
+                unset($_SESSION['no-category-found']);
+            }
+
+            if(isset($_SESSION['update']))
+            {
+                echo $_SESSION['update'];
+                unset($_SESSION['update']);
+            }
+
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+
+            if(isset($_SESSION['failed-remove']))
+            {
+                echo $_SESSION['failed-remove'];
+                unset($_SESSION['failed-remove']);
+            }
+        
+        ?>
+        <br><br>
+
+                <!-- Button to Add Admin -->
+                <a href="<?php echo SITEURL; ?>admin/add-category.php" class="btn-primary">Add Category</a>
+
+                <br /><br /><br />
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>Numri Serik</th>
+                        <th>Titulli</th>
+                        <th>Fotografia</th>
+                        <th>Featured</th>
+                        <th>Active</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <?php 
+
+                        //Query to Get all Categories from Database
+                        $sql = "SELECT * FROM kategoria";
+
+                        //Execute Query
+                        $res = mysqli_query($conn, $sql);
+
+                        //Count Rows
+                        $count = mysqli_num_rows($res);
+
+                        //Create Serial Number Variable and assign value as 1
+                        $sn=1;
+
+                        //Check whether we have data in database or not
+                        if($count>0)
+                        {
+                            //We have data in database
+                            //get the data and display
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                $id = $row['id'];
+                                $titulli = $row['titulli'];
+                                $image_name = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $sn++; ?>. </td>
+                                        <td><?php echo $titulli; ?></td>
+
+                                        <td>
+
+                                            <?php  
+                                                //Chcek whether image name is available or not
+                                                if($image_name!="")
+                                                {
+                                                    //Display the Image
+                                                    ?>
+                                                    
+                                                    <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" width="100px" >
+                                                    
+                                                    <?php
+                                                }
+                                                else
+                                                {
+                                                    //Display the Message
+                                                    echo "<div class='error'>Fotografia nuk eshte shtuar!</div>";
+                                                }
+                                            ?>
+
+                                        </td>
+
+                                        <td><?php echo $featured; ?></td>
+                                        <td><?php echo $active; ?></td>
+                                        <td>
+                                            <a href="<?php echo SITEURL; ?>admin/update-category.php?id=<?php echo $id; ?>" class="btn-secondary">Update Category</a>
+                                            <a href="<?php echo SITEURL; ?>admin/delete-category.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Category</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+
+                            }
+                        }
+                        else
+                        {
+                            //We do not have data
+                            //We'll display the message inside table
+                            ?>
+
+                            <tr>
+                                <td colspan="6"><div class="error">Nuk eshte shtuar kategoria!</div></td>
+                            </tr>
+
+                            <?php
+                        }
+                    
+                    ?>
+     </table>
+    </div>    
+</div>
+<br><br>
+<?php include('partial/footer.php'); ?>
