eb1c8e471b80858e7d1612cea6547c9898c1d7d0
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2021-03-15 01:08:47 +0100

    shtimi i manage food

---
+++ admin/manage-food.php
@@ -0,0 +1,124 @@
+<?php include('partial/logo-part.php'); ?>
+<br>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Menaxhimi i Menus</h1>
+
+        <br /><br />
+
+                <!-- Button to Add Admin -->
+                <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
+
+                <br /><br /><br />
+
+                <?php 
+                    if(isset($_SESSION['add']))
+                    {
+                        echo $_SESSION['add'];
+                        unset($_SESSION['add']);
+                    }
+
+                    if(isset($_SESSION['delete']))
+                    {
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
+
+                    if(isset($_SESSION['upload']))
+                    {
+                        echo $_SESSION['upload'];
+                        unset($_SESSION['upload']);
+                    }
+
+                    if(isset($_SESSION['unauthorize']))
+                    {
+                        echo $_SESSION['unauthorize'];
+                        unset($_SESSION['unauthorize']);
+                    }
+
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+                
+                ?>
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>Numri Serik</th>
+                        <th>Titulli</th>
+                        <th>Cmimi</th>
+                        <th>Fotografia</th>
+                        <th>Featured</th>
+                        <th>Active</th>
+                        <th>Veprimet</th>
+                    </tr>
+
+                    <?php 
+                        //Create a SQL Query to Get all the Food
+                        $sql = "SELECT * FROM ushqimi";
+
+                        $res = mysqli_query($conn, $sql);
+
+                        $count = mysqli_num_rows($res);
+
+                        $sn=1;
+
+                        if($count>0)
+                        {
+                            
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                $id = $row['id'];
+                                $titulli = $row['titulli'];
+                                $cmimi = $row['cmimi'];
+                                $image_name = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $sn++; ?>. </td>
+                                    <td><?php echo $titulli; ?></td>
+                                    <td>$<?php echo $cmimi; ?></td>
+                                    <td>
+                                        <?php  
+                                            if($image_name=="")
+                                            {
+                                                echo "<div class='error'>Fotografia nuk eshte shtuar.</div>";
+                                            }
+                                            else
+                                            {
+                                                ?>
+                                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+                                                <?php
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Food</a>
+                                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
+                                    </td>
+                                </tr>
+
+                                <?php
+                            }
+                        }
+                        else
+                        {
+                            //Food not Added in Database
+                            echo "<tr> <td colspan='7' class='error'> Ushqimi nuk eshte shtuar ende!</td> </tr>";
+                        }
+
+                    ?>
+
+                    
+                </table>
+    </div>
+    
+</div>
+<br><br>
+<?php include('partial/footer.php'); ?>
