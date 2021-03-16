c22a239ec3878bd1d7e3e413d3e34e95db65304e
diellzahodaj <75452866+diellzahodaj@users.noreply.github.com>
2021-03-16 14:13:41 +0100

    shtmi header

---
+++ partials/header.php
@@ -0,0 +1,95 @@
+<?php
+
+    include('config/constants.php');
+
+?>
+
+<html>
+
+ 
+
+<head>
+
+<meta charset="UTF-8">
+
+    <!-- Important to make website responsive -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Foodie Paradise - Menu</title>
+
+ 
+
+    <meta name="viewport" content="initial-scale=1.0 width=device-width">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Antic&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+ 
+
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+
+    <link rel='stylesheet' type='text/css' href='css/style.css'>
+
+</head>
+
+ 
+
+<body>
+
+ 
+
+    <header class="main-header">
+
+        <div class="container">
+
+            <h1 class="mh-logo">
+
+                <img src="image/logo2.2.png" width="350px" height="150px" alt="Foodie Paradise logo">
+
+            </h1>
+
+            <nav class="main-nav">
+
+                <ul class="main-nav-list">
+
+                <li>
+
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="<?php echo SITEURL; ?>menu.php">Menu</a>
+
+                   </li>
+
+                    <li>
+
+                        <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="#">Contact</a>
+
+                    </li>
+
+                </ul>
+
+            </nav>
+
+        </div>
+
+        <div class="clearfix"></div>
