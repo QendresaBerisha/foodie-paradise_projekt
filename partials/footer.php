57c917ab22e0195c8e5e4d208fe5731008ffeac3
diellzahodaj <75452866+diellzahodaj@users.noreply.github.com>
2021-03-16 14:13:26 +0100

    shtimi footer

---
+++ partials/footer.php
@@ -0,0 +1,71 @@
+ 
+
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+
+<link rel='stylesheet' type='text/css' href='../css/menustyle.css'>
+
+
+
+<div class="footer">
+
+<a href="#" class="fa fa-facebook"></a>
+
+<a href="#" class="fa fa-twitter"></a>
+
+<a href="#" class="fa fa-snapchat"></a>
+
+<a href="#" class="fa fa-instagram"></a>
+
+</div>
+
+<br><br>
+
+<footer>
+
+<div class="footer-f">
+
+<div class="title-f">
+
+  <h3>Foodie Paradise</h3>
+
+</div>
+
+
+
+  <ul class="ul-f">
+
+    <li>ADDRESS:
+
+      <span>Zona Industriale (Veternik), Prishtinë 10000</span>
+
+    </li>
+
+    <li>PHONE:
+
+      <span>+383 49 000 000</span>
+
+    </li>
+
+    <li>E-MAIL:
+
+      <span>foodieparadise@gmail.com</span>
+
+
+
+    </li>
+
+  </ul>
+
+  <div class="footer-bottom">
+
+    &copy; foodieparadise.com | ALL RIGHTS RESERVED: FOODIE PARADISE
+
+  </div>
+
+</div>
+
+
+
+</footer>
